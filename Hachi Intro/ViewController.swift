6edc237d8be8a2fb8a68ea08ac889c1261d712b9
Rona Tsai <ronatsai@2001-b011-30e0-3f38-1070-cfc4-80ca-818d.dynamic-ip6.hinet.net>
2018-11-10 10:15:48 +0800

    Initial Commit

---
+++ Hachi Intro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hachi Intro
+//
+//  Created by Rona Tsai on 2018/11/10.
+//  Copyright © 2018 Rona Tsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
